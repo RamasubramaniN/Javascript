470763ede856d4d064147d301c82627cbb0eda94
rn5 <ramasubramani_n@intuit.com>
2019-11-02 16:13:14 +0530

    Adding Es6 features.

--- ES6Features.js
+++ ES6Features.js
@@ -122,5 +122,93 @@ function setsExample() {
 
 }
 
+function staticMethodExample() {
+	console.log("********Static method start********");
+
+	StaticMethodExample.someMethod();
+
+	console.log("********Static method end********");
+}
 
 
+//No function keyword is used. No need to create instance when calling a static method.
+class StaticMethodExample {
+	static someMethod() {
+		console.log("This is a static method.")
+	}
+}
+
+function getterSetterExample() {
+	console.log("********Getter Setter start********");
+
+	let player = new Player(27);
+	console.log(player.Age);
+	player.Age = 30;
+	console.log(player.Age);
+	
+	console.log("********Getter Setter end********");
+}
+
+/*In the above example, you can see there are two functions inside class People with ‘get’ and ‘set’ properties. 
+The ‘get’ property is used to get the value of the variable and ‘set’ property is used to set the value to the
+variable. And you can see that getName function is called without parenthesis. And setName function is called 
+without parenthesis and it’s just like assigning a value to the variable.*/
+class Player {
+	constructor(age) {
+		this.age = age;
+	}
+
+	get Age() {
+		console.log("Getter invoked");
+		return this.age;
+	}
+
+	set Age(age) {
+		console.log("Setter invoked");
+		this.age = age;
+	}
+
+}
+
+/*Spread attributes help to spread the expression as the name suggests. In simple words, 
+it converts a list of elements to an array and vice versa.*/
+function spreadAttributesExample() {
+
+	console.log("********Spread Attributes start********");
+
+	//Older way. You need to pass an array.
+	sumWithoutSpreadAttributesFeature([1, 2, 3, 4, 5]); 
+	
+	//Newer way. You pass 5 parameters and this will be converted to array automatically.
+	//Just like boxing & unboxing.
+	sumWithSpreadAttributesFeature(1, 2, 3, 4, 5); 
+
+	//Max method accepts n parameters.
+	let maxOlderWay = Math.max(1, 2, 3, 4, 5, 6); // returns 200.
+	console.log("Older Way max : ", maxOlderWay)
+
+	//Convert
+	let arr = [1, 2, 3, 4, 5, 6];
+	//Math.max(arr); // Shows error. Doesn't accept an array.
+	let maxNewerWay = Math.max(...arr); //Converts array to list of elements.
+	console.log("Newer Way max : ", maxNewerWay);
+
+	console.log("********Spread Attributes end********");
+
+}
+
+let sumWithoutSpreadAttributesFeature = (arr) => {
+	let sum = 0;
+	for(let element of arr) {
+		sum += element;
+	}
+	console.log("Sum without spread attributes : " + sum);
+}
+
+let sumWithSpreadAttributesFeature = (...arr) => {
+	let sum = 0;
+	for(let element of arr) {
+		sum += element;
+	}
+	console.log("Sum with spread attributes : " + sum);
+}
